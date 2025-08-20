9feb701f1a97a0ecfbe04aca4eaf1670ddd6e761
SyNmaxtechnology <soporte@synmaxtechnology.com>
2025-08-20 15:55:16 -0600

    cambios en envio de facturas

--- subprocesos/index.js
+++ subprocesos/index.js
@@ -7,30 +7,23 @@ let contador = 0;//com
 
 setInterval(() => {
     
-    //contador ++;
+    ///contador ++;
     contador=1;///AGREGADO X SYN
     if(contador === 1) {
-        //tipo = 'NotaCredito';
-        tipo = 'Entradas';
-       // tipo = 'Facturas';
-        contador =0;
+        tipo = 'Facturas';
+        contador ++;
     } else if(contador === 2) {
-        //tipo = 'FacturasSinCorreo';
-        //tipo = 'Facturas2'; 
-        contador = 1;  
-    }/* else if (contador === 3) {
         tipo = 'FacturasSinCorreo';
-     //   contador = 0;
+        contador ++;  
+    } else if (contador === 3) {
+        tipo = 'Entradas';
+        contador ++;
          
     } else if (contador === 4) {
         tipo = 'NotaCredito';
-     //   contador = 0;
+        contador = 1;
     } 
-    else if(contador === 5){
-        tipo = 'Entradas';
-     
-        
-    }*/
+    
     child.send(tipo);
     child.on('message', (message) => {
         console.log('Iniciando subproceso', tipo);
